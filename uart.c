d05f0c83a162e9d55c31d204324f79c0d8e1951a
oranhuang <oranhuang@gmail.com>
2015-03-19 16:28:13 +0800

    modify the code 20150319

--- uart.c
+++ uart.c
@@ -13,8 +13,10 @@
 #define AVAILABLE	0
 #define TRUE		1
 #define FALSE		0
-//char *str = "curl -H "Content-Type: application/json" -d '{"state":3}' http://parkingluck.cloudas.info/pl1/rest/parkings/012?token=MTEzMjU5OTkwOUNBQUNFZEVvc2UwY0JBSURaQW91UXpSWXVIek9zN2ZPeW9JczVVQVpDTk96UnJSSkI1cVc0Z1pBY1lCWkJSczVaQVpBUEtuYWZUUkhueXpwWFVLR1VaQ3VTSHlIbmVud09YcXk0WkIwWkI0eUptbFJoSlU1b1htODkzWGNMOG5IWTgwWEpSM0F6UlpCRFpBWkMxa1pBRGl3cVpBQ3lyWkE5ZnJBVEg4SHNqM1dUTkJKcGJJMzlDUzQ1c1hEeVdTb3FHUPARKINGLUCKLOVEPARKING";
-//char *str = {"ad"b"};
+#define RECEIVE_ACK	0x55
+#define PARKING_INFO_ARRAY_LENGTH	60
+/**************************************************/
+//system ("curl -H \"Content-Type: application/json\" -d '{\"availablecar\":%t_available,\"reservedcar\":\"50\"}' \"http://parkingluck.cloudas.info/pl1/rest/management/PKL1000?id=10203591950055986&token=MTEzMjU5OTkwOUNBQUNFZEVvc2UwY0JBSURaQW91UXpSWXVIek9zN2ZPeW9JczVVQVpDTk96UnJSSkI1cVc0Z1pBY1lCWkJSczVaQVpBUEtuYWZUUkhueXpwWFVLR1VaQ3VTSHlIbmVud09YcXk0WkIwWkI0eUptbFJoSlU1b1htODkzWGNMOG5IWTgwWEpSM0F6UlpCRFpBWkMxa1pBRGl3cVpBQ3lyWkE5ZnJBVEg4SHNqM1dUTkJKcGJJMzlDUzQ1c1hEeVdTb3FHUPARKINGLUCKLOVEPARKING\"");
 
 char *str_1 = "curl -H \"Content-Type: application/json\" -d '{\"availablecar\":\"";
 char *str_2 = "\",\"reservedcar\":\"";
@@ -23,7 +25,11 @@ char *str_3 = "\"}' \"http://parkingluck.cloudas.info/pl1/rest/management/PKL100
 volatile sig_atomic_t get_signal_int = 0;
 int TOTAL_PARKING_SPACE = 0;
 int avai_parking_space;
+int send_2_server_flag = FALSE;
 char str_pool[512];
+int parking_info_read_ptr, parking_info_write_ptr;
+int parking_info_array[PARKING_INFO_ARRAY_LENGTH];
+int queue_task_count;
 typedef struct _Parking_lot_status{	
 	int parking_NO;
 	unsigned char status;
@@ -31,6 +37,8 @@ typedef struct _Parking_lot_status{
 }_parking_lot_status;
 
 _parking_lot_status *pk_lot;
+
+pthread_mutex_t lock;
 
 void get_sigint(int a)
 {
@@ -63,7 +71,7 @@ void *uart_rev(){
 	//											immediately with a failure status if the output can't be written immediately.
 	//
 	//	O_NOCTTY - When set and path identifies a terminal device, open() shall not cause the terminal device to become the controlling terminal for the process.
-	uart0_filestream = open("/dev/ttyAMA0", O_RDONLY | O_NOCTTY);// | O_NDELAY);		//Open in non blocking read/write mode
+	uart0_filestream = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY);// | O_NDELAY);		//Open in non blocking read/write mode, O_RDONLY, O_RDWR.
 	if (uart0_filestream == -1)
 	{
 		//ERROR - CAN'T OPEN SERIAL PORT
@@ -128,12 +136,11 @@ void *uart_rev(){
 	//----- CHECK FOR ANY RX BYTES -----
 	#endif
 	unsigned char rx_buffer[255];
+	unsigned char ack_buffer[6] = {0x0, 0x0, 0xF0, 0xF0, 0xA0, 0xA0};
 	unsigned char rx_data;
+
 	int rx_length, i=0, ret;
 	int end_cnt=0, data_acquire=0;
-	char str_available[8], str_reserved[8];
-	memset(str_available, 0x0, 8);
-	memset(str_reserved, 0x0, 8);
 
 	#if 0
 	int uart0_filestream, len;
@@ -153,113 +160,59 @@ void *uart_rev(){
 			ret = select(uart0_filestream+1, &readfds, NULL, NULL, NULL);		
 	
 			if(ret > 0){
-			// Read up to 255 characters from the port if they are there
-			//rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
-			rx_length = read(uart0_filestream, &rx_data, sizeof(rx_data));		//Filestream, buffer to store in, number of bytes to read (max)	
-			if (rx_length < 0)
-			{
-				//An error occured (will occur if there are no bytes)
-				printf("error occured....%s\n", strerror(errno));
-			}
-			else if (rx_length == 0)
-			{
-				//No data waiting
-				//fcntl(uart0_filestream, F_SETFL, 0);
-				//printf("wait for data....\n");
-				//printf("error occured....%s\n", strerror(errno));
-			}
-			else  
-			{
-				//Bytes received
-				//rx_buffer[rx_length] = '\0';
-				//printf("%s\n", rx_buffer);				
-				//printf("data=%d, rx len=%d\n", (int)(rx_buffer[0]*256) + (int)(rx_buffer[1]), rx_length);
-				//for(i=0;i<255;i++)
-				//	rx_buffer[i] = 0;
-				#if 1
-				rx_buffer[i] = rx_data;
-				//fprintf(stderr, "data = %x, index=%d\n", rx_data, i);
-				i++;				
-				switch (rx_data){
-					/* check the tail of the data , that's the 0xF0 0xF0 0xA0 0xA0*/
-					case 0xF0 :
-						end_cnt ++;					
-						if(end_cnt > 2)
-							end_cnt = 1;
-						break;
-					case 0xA0 :
-						if(end_cnt == 2){							
-							end_cnt++;
-						}
-						if(end_cnt == 3){
-							data_acquire = 1;
-							end_cnt = 0;
-						}
-						break;
-					default :				
-					break;
+				// Read up to 255 characters from the port if they are there
+				//rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
+				rx_length = read(uart0_filestream, &rx_data, sizeof(rx_data));		//Filestream, buffer to store in, number of bytes to read (max)	
+				if (rx_length < 0){
+					//An error occured (will occur if there are no bytes)
+					printf("error occured....%s\n", strerror(errno));
 				}
-				if(data_acquire){
-					//printf("%s", rx_buffer);
-					//for(j=0;j<2;j++)
-					data_acquire = 0;
-					//sum = (int)(rx_buffer[i-5]*256) + (int)(rx_buffer[i-4]);
-					//printf("the abs value = %d, len=%d, %x %x, \n",sum ,i, rx_buffer[i-5],rx_buffer[i-4]);//,rx_buffer[i-7], rx_buffer[i-5], rx_buffer[i-4],rx_buffer[i-3] ,rx_buffer[i-2],rx_buffer[i-1]);
-					//printf("magnetic abs value = %4.4fGa\n", (float)sum*0.92);
-					//i=0;
-				
-					if(rx_buffer[i-4] == 0x01){
-						//fprintf(stderr, "NO.%d parking lot, Is Used Now \n", rx_buffer[i-5]);
-						if(!(pk_lot + rx_buffer[i-5])->ack){
-							(pk_lot + rx_buffer[i-5])->status = USED;
-							(pk_lot + rx_buffer[i-5])->ack = TRUE;
-							avai_parking_space--;
-							fprintf(stderr, "NO.%d parking lot, Is Used Now \n", rx_buffer[i-5]);
-						}
-					}
-					else{
-						if((pk_lot + rx_buffer[i-5])->ack){
-							(pk_lot + rx_buffer[i-5])->status = AVAILABLE;
-							(pk_lot + rx_buffer[i-5])->ack = FALSE;
-						}
-						fprintf(stderr, "NO.%d parking lot, Is Available Now \n", rx_buffer[i-5]);
-						avai_parking_space++;
-						if(avai_parking_space > TOTAL_PARKING_SPACE)
-							avai_parking_space = TOTAL_PARKING_SPACE;						
-					}
-					i = 0;
-	
-						memset(str_pool, 0x0, sizeof(str_pool));
-						sprintf(str_available, "%d", avai_parking_space);
-						sprintf(str_reserved, "%d", TOTAL_PARKING_SPACE - avai_parking_space);
-						str_concate(str_1);
-						str_concate(str_available);
-						str_concate(str_2);
-						str_concate(str_reserved);
-						str_concate(str_3);
-						//fprintf(stderr, "%s\n", str_pool);
-						system(str_pool);
-						//system ("curl -H \"Content-Type: application/json\" -d '{\"availablecar\":%t_available,\"reservedcar\":\"50\"}' \"http://parkingluck.cloudas.info/pl1/rest/management/PKL1000?id=10203591950055986&token=MTEzMjU5OTkwOUNBQUNFZEVvc2UwY0JBSURaQW91UXpSWXVIek9zN2ZPeW9JczVVQVpDTk96UnJSSkI1cVc0Z1pBY1lCWkJSczVaQVpBUEtuYWZUUkhueXpwWFVLR1VaQ3VTSHlIbmVud09YcXk0WkIwWkI0eUptbFJoSlU1b1htODkzWGNMOG5IWTgwWEpSM0F6UlpCRFpBWkMxa1pBRGl3cVpBQ3lyWkE5ZnJBVEg4SHNqM1dUTkJKcGJJMzlDUzQ1c1hEeVdTb3FHUPARKINGLUCKLOVEPARKING\"");
-						fprintf(stderr, "\n");
-
-						//system("curl -H \"Content-Type: application/json\" -d '{\"availabecar\":str_available, \"reservedcar\",str_reserved}' \"http://parkingluck.cloudas.info/pl1/rest/management/PKL1000?id=10203591950055986&token=MTEzMjU5OTkwOUNBQUNFZEVvc2UwY0JBSURaQW91UXpSWXVIek9zN2ZPeW9JczVVQVpDTk96UnJSSkI1cVc0Z1pBY1lCWkJSczVaQVpBUEtuYWZUUkhueXpwWFVLR1VaQ3VTSHlIbmVud09YcXk0WkIwWkI0eUptbFJoSlU1b1htODkzWGNMOG5IWTgwWEpSM0F6UlpCRFpBWkMxa1pBRGl3cVpBQ3lyWkE5ZnJBVEg4SHNqM1dUTkJKcGJJMzlDUzQ1c1hEeVdTb3FHUPARKINGLUCKLOVEPARKING\"");
-				#if 0
-							
-					if(sum > 4000 && !current_st_is_ocupied){
-						current_st_is_ocupied = 1;
-						system("curl -H \"Content-Type: application/json\" -d '{\"state\":3}' http://parkingluck.cloudas.info/pl1/rest/parkings/012?token=MTEzMjU5OTkwOUNBQUNFZEVvc2UwY0JBSURaQW91UXpSWXVIek9zN2ZPeW9JczVVQVpDTk96UnJSSkI1cVc0Z1pBY1lCWkJSczVaQVpBUEtuYWZUUkhueXpwWFVLR1VaQ3VTSHlIbmVud09YcXk0WkIwWkI0eUptbFJoSlU1b1htODkzWGNMOG5IWTgwWEpSM0F6UlpCRFpBWkMxa1pBRGl3cVpBQ3lyWkE5ZnJBVEg4SHNqM1dUTkJKcGJJMzlDUzQ1c1hEeVdTb3FHUPARKINGLUCKLOVEPARKING");
-						printf("send the state=3 to server, car is here\n");
-					}else if (sum <= 4000 && current_st_is_ocupied){
-						current_st_is_ocupied = 0;
-						system("curl -H \"Content-Type: application/json\" -d '{\"state\":1}' http://parkingluck.cloudas.info/pl1/rest/parkings/012?token=MTEzMjU5OTkwOUNBQUNFZEVvc2UwY0JBSURaQW91UXpSWXVIek9zN2ZPeW9JczVVQVpDTk96UnJSSkI1cVc0Z1pBY1lCWkJSczVaQVpBUEtuYWZUUkhueXpwWFVLR1VaQ3VTSHlIbmVud09YcXk0WkIwWkI0eUptbFJoSlU1b1htODkzWGNMOG5IWTgwWEpSM0F6UlpCRFpBWkMxa1pBRGl3cVpBQ3lyWkE5ZnJBVEg4SHNqM1dUTkJKcGJJMzlDUzQ1c1hEeVdTb3FHUPARKINGLUCKLOVEPARKING");
-						printf("send the state=1 to server, car is moving out\n");
-					}
-					
-				#endif
-
+				else if (rx_length == 0){			
+					//No data waiting
+					//fcntl(uart0_filestream, F_SETFL, 0);
 				}
-				#endif 
-			}
+				else {
+					//Bytes received
+					rx_buffer[i] = rx_data;
+					//fprintf(stderr, "data = %x, index=%d\n", rx_data, i);
+					i++;
+					switch (rx_data){
+						/* check the tail of the data , that's the 0xF0 0xF0 0xA0 0xA0*/
+						case 0xF0 :
+							end_cnt ++;
+							if(end_cnt > 2)
+								end_cnt = 1;
+							break;
+						case 0xA0 :
+							if(end_cnt == 2){
+								end_cnt++;
+							}
+							else if(end_cnt == 3){
+								data_acquire = 1;
+								end_cnt = 0;
+							}
+							break;
+						default :
+						break;
+					}
+					if(data_acquire){
+						data_acquire = 0;
+						end_cnt = 0;
+						parking_info_array[parking_info_write_ptr] = rx_buffer[i-6];		// parking lot NO.
+						parking_info_array[parking_info_write_ptr+1] = rx_buffer[i-5];	// parking lot status, used or available
+						fprintf(stderr, "new data receive....NO.%d parking lot, status:%d\n", rx_buffer[i-6], rx_buffer[i-5]);
+						ack_buffer[0] = rx_buffer[i-6];
+						ack_buffer[1] = RECEIVE_ACK;
+						if(write(uart0_filestream, ack_buffer, sizeof(ack_buffer)) < 0)
+							fprintf(stderr, "uart writing error!!\n");
+						parking_info_write_ptr += 2;
+						parking_info_write_ptr %= PARKING_INFO_ARRAY_LENGTH;
+						pthread_mutex_lock(&lock);
+						queue_task_count++;
+						pthread_mutex_unlock(&lock);
+						i=0;
+					}
+				}
 			} // if(ret)
 		}
 	}
@@ -270,13 +223,57 @@ void *uart_rev(){
 	return 0;
 }
 
-*void queue_task(){
+void *queue_task(){
 
+	char str_available[8], str_reserved[8];
+	memset(str_available, 0x0, 8);
+	memset(str_reserved, 0x0, 8);
+	//return;
 	while(1){
 		if(queue_task_count){
-			
-			
-			queue_task_count--;
+			// if car is occupied
+			if(parking_info_array[parking_info_read_ptr+1] == 1){
+				if((pk_lot + parking_info_array[parking_info_read_ptr])->ack == FALSE){
+					(pk_lot + parking_info_array[parking_info_read_ptr])->status = USED;
+					(pk_lot + parking_info_array[parking_info_read_ptr])->ack = TRUE;
+					avai_parking_space--;
+					send_2_server_flag = TRUE;
+					fprintf(stderr, "NO.%d parking lot, Is Used Now \n", parking_info_array[parking_info_read_ptr]);
+				}
+			}
+			// or car is available
+			else{	//check the car status,
+				if((pk_lot + parking_info_array[parking_info_read_ptr])->ack == TRUE){
+					(pk_lot + parking_info_array[parking_info_read_ptr])->status = AVAILABLE; // release status to available
+					(pk_lot + parking_info_array[parking_info_read_ptr])->ack = FALSE; // change the ack to default
+					avai_parking_space++;
+					send_2_server_flag = TRUE;
+						if(avai_parking_space > TOTAL_PARKING_SPACE)
+							avai_parking_space = TOTAL_PARKING_SPACE;				
+					fprintf(stderr, "NO.%d parking lot, Is Available Now \n", parking_info_array[parking_info_read_ptr]);
+				}	
+			}
+
+			parking_info_read_ptr += 2;
+			parking_info_read_ptr %= PARKING_INFO_ARRAY_LENGTH;
+		
+			if(send_2_server_flag == TRUE){
+				send_2_server_flag = FALSE;
+				memset(str_pool, 0x0, sizeof(str_pool));
+				sprintf(str_available, "%d", avai_parking_space);
+				sprintf(str_reserved, "%d", TOTAL_PARKING_SPACE - avai_parking_space);
+				str_concate(str_1);
+				str_concate(str_available);
+				str_concate(str_2);
+				str_concate(str_reserved);
+				str_concate(str_3);
+				//fprintf(stderr, "%s\n", str_pool);
+				system(str_pool);
+				fprintf(stderr, "\n");
+			}
+			pthread_mutex_lock(&lock);
+			queue_task_count--;	
+			pthread_mutex_unlock(&lock);			
 		}
 		usleep(1);
 	}
@@ -284,7 +281,9 @@ void *uart_rev(){
 
 int main(int argc, char* argv[]){
 	int ret;
-
+	queue_task_count = 0;
+	parking_info_read_ptr = 0, parking_info_write_ptr=0;
+	memset(parking_info_array, 0x0, sizeof(parking_info_array));
 	pthread_t id_uart_rev, id_queue_task;
 
 	if(argc < 3){
@@ -308,6 +307,13 @@ int main(int argc, char* argv[]){
 	pk_lot = malloc(sizeof(_parking_lot_status)*TOTAL_PARKING_SPACE);
 	fprintf(stderr, "total available parking lots are %d\n", avai_parking_space);
 
+	 if (pthread_mutex_init(&lock, NULL) != 0)
+         {
+                 fprintf(stderr, "\n mutex init failed\n");
+                 return 1;
+         }
+
+
 	ret = pthread_create(&id_uart_rev, NULL, uart_rev, NULL);
 	if(ret != 0){
 	        fprintf(stderr, "socket_recv thread creation fail!!!\n");
@@ -325,6 +331,7 @@ int main(int argc, char* argv[]){
 	}
 	
 	free(pk_lot);
+	pthread_mutex_destroy(&lock);
 	//pthread_join(id_uart_rev, NULL);
 
 	return 0;
